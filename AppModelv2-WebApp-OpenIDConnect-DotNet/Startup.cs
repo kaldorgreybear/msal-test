e927dab7bb70a703d50e5d7d6ed2b5b3a478ee13
Greg <gebirdwell@gmail.com>
2022-02-16 16:15:03 -0600

    security stuff

--- AppModelv2-WebApp-OpenIDConnect-DotNet/Startup.cs
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Extensions;
 
 [assembly: OwinStartup(typeof(AppModelv2_WebApp_OpenIDConnect_DotNet.Startup))]
 
@@ -18,52 +20,57 @@ namespace AppModelv2_WebApp_OpenIDConnect_DotNet
         string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
         string redirectUri = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"];
         static string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenant"];
+        string secret = "Iiv7Q~_Ddrwh2fRdJ.3fXQfje.1lM4WSj1EW2";
 
         string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, System.Configuration.ConfigurationManager.AppSettings["Authority"], tenant);
 
         public void Configuration(IAppBuilder app)
         {
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
-            
+
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                AuthenticationType = "ApplicationCookies",
-                CookieSameSite = SameSiteMode.None,
-                CookieSecure = CookieSecureOption.Never
-            }
-            );
-           
+                CookieManager = new SystemWebCookieManager()
+            });
+
             app.UseOpenIdConnectAuthentication(
-            new OpenIdConnectAuthenticationOptions
-            {
-                ClientId = clientId,
-                Authority = authority,
-                RedirectUri = redirectUri,
-                PostLogoutRedirectUri = redirectUri,
-                Scope = OpenIdConnectScope.OpenIdProfile,
-                ResponseType = OpenIdConnectResponseType.CodeIdToken,
-                TokenValidationParameters = new TokenValidationParameters()
+                new OpenIdConnectAuthenticationOptions
                 {
-                    ValidateIssuer = false,
-                    NameClaimType = "name"
-                },
-                Notifications = new OpenIdConnectAuthenticationNotifications
-                {
-                    AuthenticationFailed = OnAuthenticationFailed
-                },
-                ProtocolValidator = new OpenIdConnectProtocolValidator
-                {
-                    RequireNonce = false
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = redirectUri,
+                    RedirectUri = redirectUri,
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        AuthenticationFailed = (context) =>
+                        {
+                            return System.Threading.Tasks.Task.FromResult(0);
+
+                        }
+                    },
                 }
-            }
-        );
+            );
+
+            app.UseStageMarker(PipelineStage.MapHandler);
+
+            // This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+            app.UseStageMarker(PipelineStage.Authenticate);
         }
 
-        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> context)
+        private static string EnsureTrailingSlash(string value)
         {
-            context.HandleResponse();
-            context.Response.Redirect("/?errormessage=" + context.Exception.Message);
-            return Task.FromResult(0);
+            if (value == null)
+            {
+                value = string.Empty;
+            }
+
+            if (!value.EndsWith("/", StringComparison.Ordinal))
+            {
+                return value + "/";
+            }
+
+            return value;
         }
     }
 }
